454ab8b62e84e24fbc86b3fbdac415c27656396f
Legendoery <106183491+Legendoery@users.noreply.github.com>
2022-11-23 21:29:21 +0100

    Update NewBehaviourScript.cs

--- Assets/MyGame/Scripts/NewBehaviourScript.cs
+++ Assets/MyGame/Scripts/NewBehaviourScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ArrayUebung : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     int[] array;
 
